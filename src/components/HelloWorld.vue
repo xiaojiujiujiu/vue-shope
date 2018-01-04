90f93e4805e5a1ffdf0370b879f1a2f0081d3560
xiaojiujiujiu <410946320@qq.com>
2018-01-04 11:14:44 +0800

    'git'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+
+    <h2>{{hell}} Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -25,7 +26,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      hell:'增加内容 尝试 git 是否改变'
     }
   }
 }
